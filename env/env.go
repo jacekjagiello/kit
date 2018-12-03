4bdf8ccc33b7d7b7b26472795c53883b8bc87a4b
Jacek Jagiełło <jacekjagiello47@gmail.com>
2018-12-03 16:14:49 +0100

    Add MustString and MustBool methods

--- env/env.go
+++ env/env.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"fmt"
 	"os"
 )
 
@@ -13,6 +14,15 @@ func String(key, def string) string {
 	return def
 }
 
+// MustString returns the environment variable valid specified by the key parameter,
+// it PANIC with error if environment variable does not exist
+func MustString(key string) string {
+	if env, ok := os.LookupEnv(key); ok {
+		return env
+	}
+	panic(fmt.Errorf("env: environment variable %s does not exist", key))
+}
+
 // Bool returns the environment variable valid specified by the key parameter,
 // otherwise returning a default valid if set.
 func Bool(key string, def bool) bool {
@@ -21,4 +31,14 @@ func Bool(key string, def bool) bool {
 		return true
 	}
 	return def
+}
+
+// Bool returns the environment variable valid specified by the key parameter,
+// it PANIC with error if environment variable does not exist
+func MustBool(key string) bool {
+	env := os.Getenv(key)
+	if env == "true" || env == "TRUE" || env == "1" {
+		return true
+	}
+	panic(fmt.Errorf("env: environment variable %s does not exist", key))
 }
